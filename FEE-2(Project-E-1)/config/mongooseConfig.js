29adf41e74c0a9ccb815683ea2f9e512f8d186b0
Harshitsaddi <145003816+Harshitsaddi@users.noreply.github.com>
2024-10-24 11:28:59 +0530

    Revert "backend" sss This reverts commit 575ee4abf2ffeef1e7b27ee6b751591f3506b409.

--- FEE-2(Project-E-1)/config/mongooseConfig.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const config = require('config');
-
-mongoose.connect(`${config.get('MONGODB_URL')}/movie-booking`).then(() => {
-    console.log('Connected to MongoDB...');
-}).catch(err => {
-    console.log(err);
-});
